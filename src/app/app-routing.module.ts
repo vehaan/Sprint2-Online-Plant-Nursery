ffba0b75a204e54d44441b7cdf6c09f223ee4fa8
shubham <shubhamnagmoti0609@gmail.com>
2021-04-21 15:57:56 +0530

    crud done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddSeedComponent } from './seeds/add-seed.component';
+import { EditSeedComponent } from './seeds/edit-seed.component';
+import { SeedListComponent } from './seeds/seed-list.component';
+import { SeedComponent } from './seeds/seed.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'seeds',component:SeedListComponent},
+{path:'seeds/:id',component:SeedComponent},
+{path:'add-seed',component:AddSeedComponent},
+{path:'edit-seed/:id',component:EditSeedComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+imports: [RouterModule.forRoot(routes)],
+exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SeedListComponent } from './seeds/seed-list.component';
+import { SeedComponent } from './seeds/seed.component';
+import { AddSeedComponent } from './seeds/add-seed.component';
+import { EditSeedComponent } from './seeds/edit-seed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SeedListComponent,
+    SeedComponent,
+    AddSeedComponent,
+    EditSeedComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
